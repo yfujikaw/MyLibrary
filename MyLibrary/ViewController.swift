e2d43458390fdcc1da20d866b5225498a6c209b0
藤川洋平 <yfujikaw@fujikawayouheinoMacBook-Pro.local>
2017-08-29 05:41:00 +0900

    first commit

---
+++ MyLibrary/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyLibrary
+//
+//  Created by 藤川洋平 on 2017/08/27.
+//  Copyright © 2017年 yfujikaw123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
